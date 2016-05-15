f8afe0e17dca99742911e03f3a0de3374519e2c5
Charith Perera <ngcharithperera@gmail.com>
2016-05-15 22:02:00 +0100

    Converted to Android Studio and deleted Eclipse version

--- Eclipse_Version/gen/au/csiro/openiot/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package au.csiro.openiot;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- app/build/generated/source/buildConfig/debug/au/csiro/openiot/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/au/csiro/openiot/BuildConfig.java
@@ -5,9 +5,9 @@ package au.csiro.openiot;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "au.csiro.openiot";
+  public static final String APPLICATION_ID = "au.csiro.openiot";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "";
+  public static final String VERSION_NAME = "1.0";
 }
